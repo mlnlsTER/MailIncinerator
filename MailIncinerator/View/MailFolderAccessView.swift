234074c6bba37de214ef920c1fc0af40d6eb359d
mlnlsTER <balyuk603@gmail.com>
2025-12-17 21:45:07 +0300

    Added View with instructions on how to access the folder for the MAS version

---
+++ MailIncinerator/View/MailFolderAccessView.swift
@@ -0,0 +1,18 @@
+//
+//  MailFolderAccessView.swift
+//  MailIncinerator
+//
+//  Created by Zhdan Baliuk on 17.12.2025.
+//
+
+import SwiftUI
+
+struct MailFolderAccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MailFolderAccessView()
+}
--- MailIncineratorTests/MailCacheScannerTests.swift
+++ MailIncineratorTests/MailCacheScannerTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import MailCleaner
+@testable import MailIncinerator
 
 final class MailCacheScannerTests: XCTestCase {
     
